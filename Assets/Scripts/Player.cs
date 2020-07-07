c7998b8f8b54b764b7f5a2f2bc4c66570680ce53
Fabricio Sortica <fabricio.sortica@gmail.com>
2020-07-07 13:08:49 -0300

    created Player starting position and movement script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, -2.75f, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+    }
+}
