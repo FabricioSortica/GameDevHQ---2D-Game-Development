f6cd51d789cfc006ad9e689163c93c15195351f9
Fabricio Sortica <fabricio.sortica@gmail.com>
2020-07-07 15:58:25 -0300

    Shooting section completed (from laser setup prefab to code cleanup)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,11 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 5.0f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.15f;
+    private float _canFire = -1f;
 
 
     // Start is called before the first frame update
@@ -18,11 +23,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PlayerMovement();
+        Movement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+        
 
     }
 
-    void PlayerMovement()
+    void Movement()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
@@ -41,6 +52,14 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(11f, transform.position.y, 0);
         }
+    }
+
+    void FireLaser()
+    {
+        _canFire = Time.time + _fireRate;
+        Vector3 laserOffset = new Vector3(0,0.8f,0);
+        Instantiate(_laserPrefab, transform.position + laserOffset, Quaternion.identity);  
+        
     }
 
 
